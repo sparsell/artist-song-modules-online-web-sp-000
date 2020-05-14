f24efe281f4337ecb341a467c640f26d8746ddfa
Sonja Parsell <hello@sparsell.com>
2020-05-14 10:21:08 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -2,16 +2,19 @@ module Memorable
 
   module ClassMethods
     def reset_all
-      all.clear
+      self.all.clear
     end
 
     def count
-      all.count
+      self.all.count
     end
   end
 
   module InstanceMethods
-
+    def initialize
+      self.class.all << self
+    end
+     
   end
-
+  
 end
